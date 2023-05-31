1e95829ad01cbd2c9c55866deeb89c7b1c6ff04d
Alex Rutar <alex@rutar.org>
2023-05-31 12:11:41 +0200

    add remote installation

---
+++ test/run.fish
@@ -0,0 +1,52 @@
+# INITIALIZATION
+# --------------
+
+# set initial variables
+set --local tpr_dir /Users/alexrutar/Documents/20_29-Programming/20-Development/22.14-tpr
+set --local test_dir $tpr_dir/test
+set --local test_tempdir $test_dir/temp
+
+mkdir $test_tempdir
+cd $test_tempdir
+
+# install correct vs version from directory
+fisher remove alexrutar/tpr &> /dev/null
+fisher install $tpr_dir &> /dev/null
+
+function __tpr_test
+    set_color green
+    echo "* running: 'tpr $argv'"
+    set_color normal
+    tpr $argv
+    switch $status
+        case 0
+            set_color blue; echo "* completed with no error"; set_color normal
+        case '*'
+            set_color red; echo "* completed with error"; set_color normal
+    end
+end
+
+
+# TESTING
+# -------
+
+__tpr_test help list
+__tpr_test list
+__tpr_test install
+__tpr_test init preprint
+__tpr_test archive out.tar.gz
+__tpr_test validate
+__tpr_test build out.pdf
+__tpr_test init preprint
+ls -A
+
+# CLEANUP
+# -------
+
+# remove sessions
+rm --force --recursive $test_tempdir
+functions -e __vs_test
+
+# reinstall original vs version
+fisher remove $tpr_dir &> /dev/null
+fisher install alexrutar/tpr &> /dev/null
