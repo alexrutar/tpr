7bad9142aa53a1aead4724c04f34bcc7df931ed3
Alex Rutar <alex@rutar.org>
2023-06-10 13:52:51 +0100

    fix typo

--- completions/tpr.fish
+++ completions/tpr.fish
@@ -2,7 +2,7 @@ set -l tpr_subcommands init remote archive validate build list
 complete --command tpr --exclusive
 complete --command tpr --exclusive --short-option h --long-option help --description "Print help"
 complete --command tpr --exclusive --short-option v --long-option version --description "Print version"
-complete --command tpr --short-option C --long-option directory version --description "Specify working directory"
+complete --command tpr --short-option C --long-option directory --description "Specify working directory"
 
 complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from $tpr_subcommands" --arguments init --description "Create a new project"
 complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from $tpr_subcommands" --arguments remote --description "Create a remote repository"
