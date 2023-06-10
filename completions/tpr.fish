a467982239fa9ef55f276d2c1bff31d828589ba6
Alex Rutar <alex@rutar.org>
2023-06-10 13:52:18 +0100

    improve completions

--- completions/tpr.fish
+++ completions/tpr.fish
@@ -1,7 +1,8 @@
 set -l tpr_subcommands init remote archive validate build list
 complete --command tpr --exclusive
-complete --command tpr --exclusive --long help --description "Print help"
-complete --command tpr --exclusive --long version --description "Print version"
+complete --command tpr --exclusive --short-option h --long-option help --description "Print help"
+complete --command tpr --exclusive --short-option v --long-option version --description "Print version"
+complete --command tpr --short-option C --long-option directory version --description "Specify working directory"
 
 complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from $tpr_subcommands" --arguments init --description "Create a new project"
 complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from $tpr_subcommands" --arguments remote --description "Create a remote repository"
@@ -11,4 +12,4 @@ complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from 
 complete --command tpr --exclusive --condition "not __fish_seen_subcommand_from $tpr_subcommands" --arguments list --description "List available templates"
 
 complete --command tpr --exclusive --condition "__fish_seen_subcommand_from init" --arguments "(tpr list)" 
-# complete --command tpr --exclusive --condition "__fish_seen_subcommand_from build archive"
+complete --command tpr --force-files --condition "__fish_seen_subcommand_from build"
